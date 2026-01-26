84c0f0766079765e32b2910af0db35a9bbbf816b
shashank-77-77 <shashankmankar412@gmail.com>
2026-01-27 03:19:36 +0530

    Fix socket exports for getReciverSocketId

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,6 +1,14 @@
 import http from "http";
 import { Server } from "socket.io";
 
+/* =========================
+   Socket User Map
+   ========================= */
+const userSocketMap = {};
+
+/* =========================
+   Init Socket
+   ========================= */
 export const initSocket = (app) => {
   const server = http.createServer(app);
 
@@ -13,8 +21,33 @@ export const initSocket = (app) => {
   });
 
   io.on("connection", (socket) => {
-    console.log("Socket connected:", socket.id);
+    console.log("User connected:", socket.id);
+
+    const userId = socket.handshake.query.userId;
+
+    if (userId && userId !== "undefined") {
+      userSocketMap[userId] = socket.id;
+    }
+
+    io.emit("getOnlineUser", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+      if (userId) delete userSocketMap[userId];
+      io.emit("getOnlineUser", Object.keys(userSocketMap));
+    });
   });
+
+  // expose io globally
+  app.set("io", io);
 
   return server;
 };
+
+/* =========================
+   Helper Exports
+   ========================= */
+export const getReciverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+//
