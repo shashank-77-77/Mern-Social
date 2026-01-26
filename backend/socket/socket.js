10b4c2c46fdec67665b51e1e310394cedd63cadf
shashank-77-77 <shashankmankar412@gmail.com>
2026-01-27 02:46:15 +0530

    Render backend Linked with index.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,49 +1,20 @@
-import { Server } from "socket.io";
 import http from "http";
-
-let io;
-const userSocketMap = {};
+import { Server } from "socket.io";
 
 export const initSocket = (app) => {
   const server = http.createServer(app);
 
-  io = new Server(server, {
+  const io = new Server(server, {
     cors: {
-      origin: process.env.FRONTEND_URL || "http://localhost:5173",
+      origin: "https://mern-social-frontend-s25k.onrender.com",
       credentials: true,
+      methods: ["GET", "POST"],
     },
   });
 
   io.on("connection", (socket) => {
     console.log("Socket connected:", socket.id);
-
-    const userId = socket.handshake.query.userId;
-
-    if (userId && userId !== "undefined") {
-      userSocketMap[userId] = socket.id;
-    }
-
-    io.emit("getOnlineUser", Object.keys(userSocketMap));
-
-    socket.on("disconnect", () => {
-      console.log("Socket disconnected:", socket.id);
-
-      for (const [key, value] of Object.entries(userSocketMap)) {
-        if (value === socket.id) {
-          delete userSocketMap[key];
-          break;
-        }
-      }
-
-      io.emit("getOnlineUser", Object.keys(userSocketMap));
-    });
   });
 
   return server;
 };
-
-export const getReciverSocketId = (receiverId) => {
-  return userSocketMap[receiverId];
-};
-
-export { io };
